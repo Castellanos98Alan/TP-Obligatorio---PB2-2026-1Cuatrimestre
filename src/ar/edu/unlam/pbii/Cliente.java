e32aebac91cd5741900525f1831a8f5f22e46ac2
Salvatorum <tscanosa@gmail.com>
2026-06-24 13:34:48 -0300

    Agregar listas de reservas para clientes y clases para profesionales

--- src/ar/edu/unlam/pbii/Cliente.java
+++ src/ar/edu/unlam/pbii/Cliente.java
@@ -1,16 +1,26 @@
 package ar.edu.unlam.pbii;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cliente extends Persona{
 	
 	private static  Integer proximoId = 1;
 	private Integer id=0;
+	private ArrayList<Reserva> reservasDadas;
 
 	public Cliente(String nombre, String apellido, String dni, Integer id) {
 		super(nombre, apellido, dni);
 		this.id= proximoId++;
-
+		this.reservasDadas = new ArrayList<>();
 	}
-
+	public Boolean agregarReserva(Reserva r) {
+		return this.reservasDadas.add(r);
+	}
+	public List<Reserva> getReservasDadas(){
+		return this.reservasDadas;
+	}
+	
 	public Integer getId() {
 		return id;
 	}
